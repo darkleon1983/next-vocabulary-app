0a6f54700597486293b622afc303fc30e2ef8ade
Евгений Королев <mts9832899777@gmail.com>
2026-01-24 00:04:12 +0700

    just added parametres to stopTestButton

--- app/components/ui/StopTestButton/index.tsx
+++ app/components/ui/StopTestButton/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import cn from "classnames";
+import styles from "./stopTestButton.module.scss";
 
 const StopTestButton = () => {
-  return <button>Stop the test</button>;
+  return <button className={cn(styles.stopButton)}>Stop the test</button>;
 };
 
 export default StopTestButton;
